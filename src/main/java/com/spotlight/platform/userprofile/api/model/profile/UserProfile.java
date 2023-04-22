94c4e207ed13b64c7dda3f93696661ca17edbb27
charliecccyang <20199104+charliecccyang@users.noreply.github.com>
2023-04-22 20:09:09 +0800

    add methods to deal with the commands

--- src/main/java/com/spotlight/platform/userprofile/api/model/profile/UserProfile.java
+++ src/main/java/com/spotlight/platform/userprofile/api/model/profile/UserProfile.java
@@ -7,8 +7,27 @@ import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfi
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public record UserProfile(@JsonProperty UserId userId, @JsonProperty @JsonFormat(shape = JsonFormat.Shape.STRING) Instant latestUpdateTime,
                           @JsonProperty Map<UserProfilePropertyName, UserProfilePropertyValue> userProfileProperties) {
+
+    public void replaceProperty(UserProfilePropertyName propertyName, UserProfilePropertyValue propertyValue) {
+        userProfileProperties.put(propertyName, propertyValue);
+    }
+
+    public void incrementProperty(UserProfilePropertyName propertyName, UserProfilePropertyValue incrementValue) {
+        UserProfilePropertyValue currentValue = userProfileProperties.getOrDefault(propertyName, UserProfilePropertyValue.valueOf(0));
+        UserProfilePropertyValue newValue = UserProfilePropertyValue.valueOf(currentValue.getValueAsInt() + incrementValue.getValueAsInt());
+        userProfileProperties.put(propertyName, newValue);
+    }
+
+    public void collectProperty(UserProfilePropertyName propertyName, UserProfilePropertyValue valuesToCollect) {
+        UserProfilePropertyValue currentValue = userProfileProperties.getOrDefault(propertyName, UserProfilePropertyValue.valueOf(new ArrayList<>()));
+        List<Object> newList = new ArrayList<>(currentValue.getValueAsList());
+        newList.addAll(valuesToCollect.getValueAsList());
+        userProfileProperties.put(propertyName, UserProfilePropertyValue.valueOf(newList));
+    }
 }
