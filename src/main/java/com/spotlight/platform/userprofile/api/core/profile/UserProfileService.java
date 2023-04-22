96ccc0dc5a2d6aa41ac424753cdc3ad0acfb63b5
charliecccyang <20199104+charliecccyang@users.noreply.github.com>
2023-04-22 20:10:47 +0800

    apply the command

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
@@ -2,10 +2,13 @@ package com.spotlight.platform.userprofile.api.core.profile;
 
 import com.spotlight.platform.userprofile.api.core.exceptions.EntityNotFoundException;
 import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDao;
+import com.spotlight.platform.userprofile.api.model.commands.UserProfileCommand;
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
 
 import javax.inject.Inject;
+import java.time.Instant;
+import java.util.HashMap;
 
 public class UserProfileService {
     private final UserProfileDao userProfileDao;
@@ -17,5 +20,16 @@ public class UserProfileService {
 
     public UserProfile get(UserId userId) {
         return userProfileDao.get(userId).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void applyCommand(UserProfileCommand command) {
+        UserProfile userProfile = userProfileDao.get(command.getUserId()).orElseGet(() -> {
+            UserProfile newProfile = new UserProfile(command.getUserId(), Instant.now(), new HashMap<>());
+            userProfileDao.put(newProfile);
+            return newProfile;
+        });
+
+        command.apply(userProfile);
+        userProfileDao.put(userProfile);
     }
 }
