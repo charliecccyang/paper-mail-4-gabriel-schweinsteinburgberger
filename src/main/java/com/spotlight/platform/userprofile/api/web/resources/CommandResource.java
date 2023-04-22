b265bba3ce2b42c60e1f98475edc8ba0ab56ee47
charliecccyang <20199104+charliecccyang@users.noreply.github.com>
2023-04-22 20:19:15 +0800

    Create a new CommandResource to accept and process commands

---
+++ src/main/java/com/spotlight/platform/userprofile/api/web/resources/CommandResource.java
@@ -0,0 +1,32 @@
+package com.spotlight.platform.userprofile.api.web.resources;
+
+import com.spotlight.platform.userprofile.api.core.profile.UserProfileService;
+import com.spotlight.platform.userprofile.api.model.commands.UserProfileCommand;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users/commands")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CommandResource {
+
+    private final UserProfileService userProfileService;
+
+    @Inject
+    public CommandResource(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @POST
+    public Response processCommand(@Valid UserProfileCommand command) {
+        userProfileService.applyCommand(command);
+        return Response.accepted().build();
+    }
+}
