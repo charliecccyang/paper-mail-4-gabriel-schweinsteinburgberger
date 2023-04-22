e64be58ca77124e67102b6b7528bee1af91c0394
charliecccyang <20199104+charliecccyang@users.noreply.github.com>
2023-04-22 19:44:49 +0800

    User profiles can be created on the fly when user is first time seen by the service

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
@@ -3,6 +3,8 @@ package com.spotlight.platform.userprofile.api.core.profile.persistence;
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,7 +14,15 @@ public class UserProfileDaoInMemory implements UserProfileDao {
 
     @Override
     public Optional<UserProfile> get(UserId userId) {
-        return Optional.ofNullable(storage.get(userId));
+
+        final UserProfile userProfile = storage.get(userId);
+        if(userProfile != null){
+            return Optional.of(userProfile);
+        }
+
+        final UserProfile newProfile = new UserProfile(userId, Instant.now(), new HashMap<>());
+        put(newProfile);
+        return Optional.of(newProfile);
     }
 
     @Override
